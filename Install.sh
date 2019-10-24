057b81f2a7ba70395f4ac769e32a07240ab86df7
AmirSedrak <Arsedrak@gmail.com>
2019-10-24 11:06:27 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,12 +1,12 @@
 oc login -u admin -p openshift-demo
 
 
-oc new-project ci-cd-test2 \
+oc new-project Pipeline \
             --description="This is an example project to demonstrate OpenShift CI/CD automation" \
                 --display-name="CI/CD Test "
 
 
-oc project ci-cd-test2
+oc project Pipeline
 
 
 oc new-app -f sonar/sonarqube-postgresql-template.yaml --param=SONARQUBE_VERSION=6.7
@@ -20,9 +20,9 @@ oc new-app -f selenium/selenium-node-chrome.yaml
 oc new-app -f selenium/selenium-node-firefox.yaml
 
 
+ 
 
-
-oc new-app --name=testing-pipeline wildfly~https://github.com/openshiftdemos/os-sample-java-web.git
+oc new-app  wildfly~https://github.com/openshiftdemos/os-sample-java-web.git    --name=testing-pipeline
 oc expose svc/testing-pipeline
 
 
