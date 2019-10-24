23e2baf0fe1636df9ba267ed99e1421a4dc6203e
AmirSedrak <Arsedrak@gmail.com>
2019-10-23 20:20:17 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -9,23 +9,17 @@ oc new-project ci-cd-test2 \
 oc project ci-cd-test2
 
 
-git clone https://github.com/AmirSedrak/test.git
-
-cd test/sonar/
-
-oc new-app -f sonarqube-postgresql-template.yaml --param=SONARQUBE_VERSION=6.7
-
-cd ..
-
-cd selenium/
+oc new-app -f sonar/sonarqube-postgresql-template.yaml --param=SONARQUBE_VERSION=6.7
 
 
-oc create -f selenium-hub.yaml
 
-oc create -f selenium-node-chrome.yaml
-oc create -f selenium-node-firefox.yaml
 
-cd ..
+oc create -f selenium/selenium-hub.yaml
+
+oc create -f selenium/selenium-node-chrome.yaml
+oc create -f selenium/selenium-node-firefox.yaml
+
+
 
 
 oc new-app --name=testing-pipeline wildfly~https://github.com/openshiftdemos/os-sample-java-web.git
