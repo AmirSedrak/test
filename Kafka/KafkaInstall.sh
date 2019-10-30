14be5a38c4cd98264400a420ce1d7ad91006c598
AmirSedrak <Arsedrak@gmail.com>
2019-10-30 19:12:59 -0400

    Update KafkaInstall.sh

--- Kafka/KafkaInstall.sh
+++ Kafka/KafkaInstall.sh
@@ -6,13 +6,12 @@ oc new-project myproject  --description="This is an example project to demonstra
 
 
 
-
 oc apply -f strimzi-cluster-operator.yaml -n myproject
 
 oc apply -f kafka.yml -n myproject
 
 
 
-oc -n myproject run kafka-producer -ti --image=strimzi/kafka:0.14.0-kafka-2.3.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list my-cluster-kafka-bootstrap:9092 --topic my-topic
 
+oc wait kafka/my-cluster --for=condition=Ready --timeout=300s -n myproject
 
